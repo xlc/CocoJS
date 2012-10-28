883c5d18c785bdd0d004e173b9872c852e9eb6b6
Xiliang Chen <xlchen1291@gmail.com>
2012-10-28 21:49:00 +1300

    add function to create structs

--- CocoJS/scripts/main.js
+++ CocoJS/scripts/main.js
@@ -1,24 +1,23 @@
-NSObject = objc.NSObject
+function Point(x, y) {
+    if (typeof x != 'number' || typeof y != 'number')
+        throw TypeError('invalid input type. x: ' + x + ', y: ' + y);
+    return {x:x, y:y};
+}
 
-Parent = NSObject.extend('Parent', null, {
-    init: function() {
-        this._super();
-        log('parent');
-    }, 
-    test: function() {
-        log('parent test');
-    },
-    t: 1
-});
+function Size(width, height) {
+    if (typeof width != 'number' || typeof height != 'number')
+        throw TypeError('invalid input type. width: ' + width + ', height: ' + height);
+    return {width:width, height:height};
+}
 
+function Rect(x, y, width, height) {
+    if (typeof x != 'number' || typeof y != 'number' || typeof width != 'number' || typeof height != 'number')
+        throw TypeError('invalid input type. x: ' + x + ', y: ' + y + ', width: ' + width + ', height: ' + height);
+    return {x:x, y:y, width:width, height:height};
+}
 
-Child = Parent.extend('Child', null, {
-    init: function() {
-        this._super();
-        log('Child');
-    },
-    test: function() {
-        log('child test');
-        this._super();
-    },
-});
+function Range(location, length) {
+    if (typeof location != 'number' || typeof length != 'number')
+        throw TypeError('invalid input type. location: ' + location + ', length: ' + length);
+    return {location:location, length:length};
+}
